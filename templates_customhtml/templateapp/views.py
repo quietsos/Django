e63bf842eff7fc8ac9be28224380bf1ad73b8959
Md Shohanuzzaman <shohaniu8@gmail.com>
2022-04-05 23:30:35 +0600

    template and customhtml

---
+++ templates_customhtml/templateapp/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+
+
+
+
+def home(request, year=datetime.now().year, month=datetime.now().strftime("%B")):
+    name = "sohan"
+    month = month.title()
+
+    #Convert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    # Create a calender
+    cal = HTMLCalendar().formatmonth(
+        year, 
+        month_number,
+        
+
+    )
+
+    #Get current year
+
+    now = datetime.now()
+    current_year = now.year
+
+    #Get current time
+    time = now.strftime("%I:%M:%S %p")
+
+
+    return render(request, 'templateapp/home.html',{
+        "name" : name,
+        "year":year,
+        "month":month,
+        "month_number": month_number,
+        "cal":cal,
+        "current_year": current_year,
+        "time":time,
+
+
+
+
+
+    })
+
